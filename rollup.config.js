cda5114497fc4fe7e843f396155251be12812fbb
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2025-12-24 14:34:35 +0000

    commited first package

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from "rollup-plugin-typescript2";
+import del from "rollup-plugin-delete";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "lib/index.esm.js",
+      format: "esm"
+    },
+    {
+      file: "lib/index.cjs",
+      format: "cjs"
+    }
+  ],
+  plugins: [
+    del({
+      targets: "lib/*"
+    }),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfig: "tsconfig.json"
+    })
+  ]
+};
