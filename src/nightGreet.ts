e04fcd29ace632c8efc6fefbdd6852f0efa46ccd
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2025-12-26 14:26:00 +0000

    updatd good night greeting

--- src/nightGreet.ts
+++ src/nightGreet.ts
@@ -1,3 +1,3 @@
 export const nightGreet = (name: string): string => {
-  return `Good night, ${name}! Have a great night ahead.`;
+  return `Good night and sweet dreams, ${name}! Have a great night ahead.`;
 };
